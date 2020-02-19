30f6d96373d94a623953b05e8c1bc7ad25e0ca09
SEQUOiA87 <10127637+SEQUOiA87@users.noreply.github.com>
2020-02-19 02:11:08 +0100

    Make it work for German device locale, too

--- app/src/main/java/com/duck/stayawakeadb/ADBNotificationListener.kt
+++ app/src/main/java/com/duck/stayawakeadb/ADBNotificationListener.kt
@@ -7,6 +7,7 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 /**
  * Created by Bradley Duck on 2019/02/18.
+ * Improved by Andreas Hellwig on 2020/02/18.
  */
 class ADBNotificationListener : android.service.notification.NotificationListenerService() {
 
@@ -18,33 +19,31 @@ class ADBNotificationListener : android.service.notification.NotificationListene
     }
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
-        if (helperUtil.developerOptionsEnabled && helperUtil.usbDebuggingEnabled) {
-            if (sbn.packageName.equals("android", ignoreCase = true)) {
-                val notification = sbn.notification
-                val title = notification.extras.getString("android.title")
-                if (title != null && title.equals("USB debugging connected", ignoreCase = true)) {
-                    setAndSendBroadcast(true)
-                }
-            }
-        }
+        checkNotification(sbn) { setAndSendBroadcast(true) }
     }
 
     override fun onNotificationRemoved(sbn: StatusBarNotification) {
-        if (helperUtil.developerOptionsEnabled && helperUtil.usbDebuggingEnabled) {
-            if (sbn.packageName.equals("android", ignoreCase = true)) {
-                val notification = sbn.notification
-                val title = notification.extras.getString("android.title")
-                if (title != null && title.equals("USB debugging connected", ignoreCase = true)) {
-                    setAndSendBroadcast(false)
-                }
-            }
+        checkNotification(sbn) { setAndSendBroadcast(false) }
+    }
+
+    private fun checkNotification(sbn: StatusBarNotification, onPositiveCheck: () -> Unit) {
+        if (helperUtil.developerOptionsEnabled && helperUtil.usbDebuggingEnabled
+            && sbn.packageName.equals("android", ignoreCase = true)
+        ) {
+            val title = sbn.notification.extras.getString("android.title") ?: return
+
+            if (title.equals("USB debugging connected", ignoreCase = true)
+                || title.equals("USB-Debugging aktiviert", ignoreCase = true)
+            )
+                onPositiveCheck()
         }
     }
 
     private fun setAndSendBroadcast(turnOn: Boolean) {
         if (helperUtil.setStayAwake(turnOn)) {
             //update the Activity UI if it is running...
-            LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(Intent(INTENT_ACTION))
+            LocalBroadcastManager.getInstance(applicationContext)
+                .sendBroadcast(Intent(INTENT_ACTION))
         } else {
             //todo:?
         }
